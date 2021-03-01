529e52f691e6c3553ec71aa86f3cf29132b6011e
MadhushaPrasad <madushaprasad21@gmail.com>
2021-03-01 09:29:10 +0530

    Add 2nd sem OOC Lecture 01 exercises

---
+++ 2st Sem/OOC/Exercise/lecture 01/Exercise.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "Madhusha Prasad" << std::endl;
+	std::cout << "Kalutara" << std::endl;
+	std::cout << "St.John\'s College Panadura" << std::endl;
+
+	return 0;
+}
+
